784b2fac545e38af243f4c049d6c63457088cf7c
benhassara <bhassara@gmail.com>
2015-09-02 10:35:54 -0600

    move round msgs to separate file

--- js/deck.js
+++ js/deck.js
@@ -27,3 +27,19 @@ var deck = [
   {rank: 13, unicode: "🂮"}, {rank: 13, unicode: "🂾"},
   {rank: 13, unicode: "🃎"}, {rank: 13, unicode: "🃞"}
   ];
+
+var msgs = {
+  1: 'Waterfall',
+  2: 'Pick two people, and make them drink.',
+  3: 'Three is me. Take 3 drinks.',
+  4: 'Take 2 drinks, and give out 2.',
+  5: 'Set a rule',
+  6: 'Thumb rule - Place a thumb on the table, last person to do so drinks.',
+  7: 'Hand to heaven, last person to do so drinks.',
+  8: 'Choose a mate, every time they drink, you drink.',
+  9: 'Rhyme time - Pick a word, everyone rhymes against it, if you get stumped you drink.',
+  10: 'Categories, pick a category, if you get stumped..you drink.',
+  11: 'Dudes drink...',
+  12: 'Ladies drink...',
+  13: "King. If it isn't the last king, add to kings cup. If last King, drink kings cup."
+};
--- js/decktest.js
+++ js/decktest.js
@@ -21,7 +21,7 @@ function addCard(cardChar, msg) {
 
 // Pull first card in array and call styling function "addCard"
 
-function cardDiscard() {
+function drawCard() {
     //if shufDeck empty, func will return undefined
     if (shufDeck[0] !== undefined) {
       var picked = shufDeck[0].unicode;
@@ -36,7 +36,7 @@ function cardDiscard() {
 function showCard(num){
 
     for (var i = 0; i < num; i++) {
-      var current = cardDiscard();
+      var current = drawCard();
       if (current !== undefined) {
         var msg = (playRound(current.rank));
         addMessage(msg);
@@ -62,68 +62,10 @@ function addButton() {
 
 function playRound(rank) {
 
-  var result = "";
+  if (rank >= 1 && rank <= 13)
+    return msgs[rank.toString()];
 
-  switch(rank) {
-
-    case 1:
-      result = "Waterfall";
-      break;
-
-    case 2:
-      result = "Pick two people, and make them drink.";
-      break;
-
-    case 3:
-      result = "Three is me, take 3 drinks.";
-      break;
-
-    case 4:
-      result = "Take 2 drinks, give out 2.";
-      break;
-
-    case 5:
-      result = "Set a rule to follow.";
-      break;
-
-    case 6:
-      result = "Thumb rule - Place a thumb on the table, last person to do so drinks.";
-      break;
-
-    case 7:
-      result = "Hand to heaven, last person to do so drinks.";
-      break;
-
-    case 8:
-      result = "Choose a mate, every time they drink, you drink.";
-      break;
-
-    case 9:
-      result = "Rhyme time - Pick a word, everyone rhymes against it, if you get stumped you drink.";
-      break;
-
-    case 10:
-      result = "Categories, pick a category, if you get stumped..you drink.";
-      break;
-
-    case 11:
-      result = "Dudes drink...";
-      break;
-
-    case 12:
-      result = "Ladies drink...";
-      break;
-
-    case 13:
-      result = "King, if it isn't the last king, add to kings cup. If last King, drink kings cup.";
-      break;
-    default:
-      result = "game over";
-      break;
-
-  }
-
-  return result;
+  return 'Game Over';
 
 }
 
